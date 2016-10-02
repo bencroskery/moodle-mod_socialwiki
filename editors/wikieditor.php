7bf5612f2c9d08ea066c6608730bf0927ec947c7
bencroskery <ben@irtech.com>
2016-10-01 23:42:50 -0400

    Moodle < 3.1 contructor fix
    - add deprecated contructor syntax used by older versions of Moodle

--- editors/wikieditor.php
+++ editors/wikieditor.php
@@ -47,6 +47,16 @@ class MoodleQuickForm_socialwikieditor extends MoodleQuickForm_textarea {
         parent::__construct($elementname, $elementlabel, $attributes);
     }
 
+    /**
+     * Old syntax of class constructor. Deprecated in PHP7.
+     *
+     * @deprecated since Moodle 3.1
+     */
+    public function MoodleQuickForm_textarea($elementName = null, $elementLabel = null, $attributes = null) {
+        debugging('Use of class name as constructor is deprecated', DEBUG_DEVELOPER);
+        self::__construct($elementName, $elementLabel, $attributes);
+    }
+
     public function setwikiformat($wikiformat) {
         $this->wikiformat = $wikiformat;
     }
